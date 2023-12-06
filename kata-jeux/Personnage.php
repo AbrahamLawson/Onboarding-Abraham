3bf4ab029d19926ff104184dde6999efe7458843
Lawson Abraham <abraham@MBP-de-Lawson.localdomain>
2023-12-06 16:16:43 +0100

    Creation of the method for display the personnage statistic

--- kata-jeux/Personnage.php
+++ kata-jeux/Personnage.php
@@ -10,6 +10,8 @@ class Personnage{
         $this->pointAttaque = $pointAttaque;
         $this->pointDefense = $pointDefense;;
     } 
-   
+    public function getStatistique():void{
+        echo "Voici les statistiques du personnage ".$this->nom.":". "<br/>"."Santé:".$this->sante."<br/>". "Point d'attaque:".$this->pointAttaque."<br/>". "Point de défense".$this->pointDefense;
+    }
 }
 
